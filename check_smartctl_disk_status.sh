8e3b574b40f8484be0fc8b58d4dd3f6a2df0cb5f
Francesco R <424577+francescor@users.noreply.github.com>
2023-02-20 12:47:44 +0300

    Adding basic script

---
+++ check_smartctl_disk_status.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Cron script that checks smartctl output for a list of disks
+# https://github.com/francescor/check_smartctl_output
+# We check smartctl exit code, and also some "useful" smartctl output as in https://askubuntu.com/questions/20393/how-do-i-interpret-hdd-s-m-a-r-t-results
+# iso: we just read value that makes sense: all values shoud return zero
+
+# list of disk to check
+DISKS_TO_CHECK='/dev/sda /dev/sdb /dev/sdc /dev/sdd'
+
+# no need to do anything below
+SMART_LINES_TO_CHECK="Offline_Uncorrectable|Reallocated_Sector_Ct|Current_Pending_Sector"
+SMARTCTL=`which smartctl`
+EGREP=`which egrep`
+UNIQ=`which uniq`
+AWK=`which awk`
+
+exit_code=0
+for disk in $DISKS_TO_CHECK; do
+  output=`$SMARTCTL -a $disk`
+  if ! [ $? -eq 0 ] ; then
+	echo "Disk: $disk"
+        echo "Error: something went wrong while executing smartctl on disk: $disk"
+	echo "The disk either does not exist, or smartctl exit code is not zero, so check it with:"
+        echo "smartctl -a $disk"
+	echo
+	exit_code=1
+  else
+    result=`echo "$output" |  $EGREP \"$SMART_LINES_TO_CHECK\" | $AWK '{print $10}' | $UNIQ`
+    if ! [ "$result" -eq "0" ] ; then
+      echo "Disk: $disk"
+      echo "Warning: smartctl returned some non zero values on meaninful output for disk: $disk"
+      echo "Check it youself with:"
+      echo "smartctl -a $disk |egrep \"$SMART_LINES_TO_CHECK\""
+      echo
+    fi
+  fi
+done
+exit $exit_code
